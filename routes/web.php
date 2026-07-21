f643ae2fa2554461b4b1f6c85814ea445f1af8e4
Nashwahrz <nashwaharzathi05@gmail.com>
2026-07-21 23:02:25 +0700

    Add explicit storage route for serverless environments

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return 'connection sucses';
 });
+  
+Route::get('/storage/{path}', function () {  = storage_path('app/public/' . ); if (file_exists()) { return response()-; } abort(404); })-, '.*'); 
